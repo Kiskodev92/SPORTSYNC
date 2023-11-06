7c15e9359470542d3cf65337819c3c4bb7caafb1
Kiskotkd <fjgp92@hotmail.com>
2023-11-06 13:03:29 +0100

    card y home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './component/card/card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
