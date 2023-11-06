81d45cc25cec8619c4bc4911821a1c96b8de0119
borjalopez025 <borjaswim1995@gmail.com>
2023-11-06 19:26:46 +0100

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormLogimComponent } from './componente/form-logim/form-logim.component';
+import { LoginComponent } from './paginas/login/login.component';
+import { HeaderComponent } from './componente/header/header.component';
+import { NavBarComponent } from './componente/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormLogimComponent,
+    LoginComponent,
+    HeaderComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componente/form-logim/form-logim.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-logim',
+  templateUrl: './form-logim.component.html',
+  styleUrls: ['./form-logim.component.css']
+})
+export class FormLogimComponent {
+
+}
