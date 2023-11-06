2f2916ed990c4a4d1cc8c23c6757ae6184cbcffe
borjalopez025 <borjaswim1995@gmail.com>
2023-11-06 20:34:33 +0100

    login2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComLoginComponent } from './component/com-login/com-login.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComLoginComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/com-login/com-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-com-login',
+  templateUrl: './com-login.component.html',
+  styleUrls: ['./com-login.component.css']
+})
+export class ComLoginComponent {
+
+}
