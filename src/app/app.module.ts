cd7e8e180a5726ab68c335cd2c51027e987362ee
borja <borjaswim1995@gmail.com>
2023-11-09 00:46:48 +0100

    Mensaje de commit que describe los cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './component/card/card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ComLoginComponent } from './component/com-login/com-login.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -15,6 +13,7 @@ import { RegisterDeporteComponent } from './pages/register-deporte/register-depo
 import { FormRegisterComponent } from './component/form-register/form-register.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { RouterModule } from '@angular/router';
+import { CardComponent } from './component/card/card.component';
 
 @NgModule({
   declarations: [
@@ -31,13 +30,16 @@ import { RouterModule } from '@angular/router';
     FormRegisterComponent,
     RegisterComponent
   ],
-  schemas:[CUSTOM_ELEMENTS_SCHEMA],
+  
   imports: [
     RouterModule,
     BrowserModule,
     AppRoutingModule
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
