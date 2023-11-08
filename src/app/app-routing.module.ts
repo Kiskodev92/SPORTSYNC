e41f436605606ab2106604b0d8ae417a88288a71
borja <borjaswim1995@gmail.com>
2023-11-08 03:46:15 +0100

    ajustes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RegisterDeporteComponent } from './pages/register-deporte/register-deporte.component';
+import { EventPageComponent } from './pages/event-page/event-page.component';
+
 
 const routes: Routes = [
-  {path: '', component:HomeComponent},
-  {path: 'login', component:LoginComponent}
+  {path: 'home', component:HomeComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'register ', component:RegisterComponent},
+  {path:'register-deporte', component:RegisterDeporteComponent},
+  {path:'even-page', component:EventPageComponent},
+  
 ];
 
 @NgModule({
