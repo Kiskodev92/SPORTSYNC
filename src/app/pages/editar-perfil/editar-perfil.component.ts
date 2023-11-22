4fc87fd8129e64b1a10774f0ac330502ba364b74
Cristinahb <achbocourt@gmail.com>
2023-11-22 12:17:58 +0100

    subir editarPerfil

--- src/app/pages/editar-perfil/editar-perfil.component.ts
+++ src/app/pages/editar-perfil/editar-perfil.component.ts
@@ -10,20 +10,48 @@ import { Router } from '@angular/router';
 })
 export class EditarPerfilComponent {
 
-  public user: User = new User(0, '', '', '', '', '', '', '');
+  public update_user: User 
 
-  constructor(private userService: UserService, public router: Router) { }
-
-
-  editar() {
+  constructor(private userService: UserService, public router: Router) {
+    this.update_user = this.userService.user || new User(null, '', null, null, null, '', '', '');
+  }
   
-      this.userService.editPerfil(this.user).subscribe(() => {
+
+  editarPerfil(nombre:string, provincia:string, descripcion:string, foto:string): void {
+
+   let actualizacion: boolean = false;
+
+   if(nombre != ''){
+   this.update_user.nombre = nombre;
+   actualizacion = true;
+   }
+
+   if(provincia != ''){
+   this.update_user.provincia = provincia;
+   actualizacion = true;
+   }
+
+   if(descripcion != ''){
+   this.update_user.descripcion = descripcion;
+   actualizacion = true;
+   }
+
+   if(foto != ''){
+   this.update_user.foto = foto;
+   actualizacion = true;
+   }
+  
+      this.userService.user = this.update_user;
+
+      this.userService.editPerfil(this.update_user).subscribe(() => {
           console.log('Perfil actualizado con éxito');
+          console.log(this.update_user);
+          
           this.router.navigate(['/perfil']);
         })
   }
 
-  cancelarPerfil(){
-      this.router.navigate(['/perfil']);
-}
+  cancelarPerfil(): void {
+    this.router.navigate(['/perfil']);
+  }
 }
