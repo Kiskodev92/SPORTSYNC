57e21fbde2c06fdde190d5f671124fd55294fb85
borja <borjaswim1995@gmail.com>
2023-11-30 10:44:28 +0100

    guardar

--- src/app/shared/eventos.service.ts
+++ src/app/shared/eventos.service.ts
@@ -16,10 +16,12 @@ export class EventosService {
 
   private url: string = 'http://localhost:3000/home'
   private url2: string = 'http://localhost:3000/explore'
+  private urlex2: string = 'http://localhost:3000/explore2'
   private url3: string = 'http://localhost:3000/evento'
   private urlsport: string = 'http://localhost:3000/sport'
   private urlbtn: string = 'http://localhost:3000/btn'
   private urlsportus: string = 'http://localhost:3000/sportus'
+  private urldelete: string = 'http://localhost:3000/delete'
 
 
   public event: Event
@@ -42,20 +44,27 @@ export class EventosService {
   }
 
   getEventos(){
-    return this.http.get(`${this.url}?provincia=${this.userService.user.provincia}`);
+    return this.http.get(`${this.url}`);
   }
 
-  getOne(titulo?:string,provincia?:string){
+  getOne(provincia: string){
 
-    if(titulo){
-      console.log('entro en el servicio titulo');
+    if(provincia !== undefined){
       
-      return this.http.get(`${this.url2}?titulo=${titulo}`)
-    }if(provincia){
-      console.log('entro en el servicio provincia');
-
-      return this.http.get(`${this.url2}?provincia=${provincia}`)
+    console.log('Buscando por provincia desde el servicio');
+    return this.http.get(`${this.url2}?provincia=${provincia}`);
+    }else{
+      console.log('busca otra manera');
+      
     }
+    
+  }
+  
+  getone2(titulo:string){
+    console.log(titulo);
+    
+    console.log('entro a titulo desde servicio');
+    return this.http.get(`${this.urlex2}?titulo=${titulo}`)
   }
 
   btnFollow(UsEvent: UsEvent){
@@ -67,5 +76,10 @@ export class EventosService {
   getSportUs(){
 
     return this,this.http.get(`${this.urlsportus}?id=${this.userService.user.id_user}`)
+  }
+
+  dejarDeSeguir(id_usuario:number,id_evento:number){
+
+    return this.http.request('delete' , this.urldelete, { body: { id :this.userService.user.id_user,id_evento}})
   }
 }
